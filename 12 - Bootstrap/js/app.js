37a6375df9b8802c786dfac4285592b3e4f74db5
Kevin Mejia <kwmejia9@gmail.com>
2023-12-12 12:41:11 -0500

    Inventario

---
+++ 12 - Bootstrap/js/app.js
@@ -0,0 +1,14 @@
+// Selectores
+const tbody = document.querySelector("#tbody");
+
+//Lista de productos
+const listaProductos = [
+    {
+        id: Date.now(),
+        nombre: "Pastas",
+        precio: 5.0,
+        cantidad: 10,
+        imagen: "",
+        categoria: "Carbohidrato"
+    }
+];

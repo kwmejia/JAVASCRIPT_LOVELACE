1a7d691aaefc4c046a5e4420f6061535a73f9a33
Kevin Mejia <kwmejia9@gmail.com>
2024-01-09 12:22:47 -0500

    Carrito corregido

--- 13 - Carrito de compras/js/app.js
+++ 13 - Carrito de compras/js/app.js
@@ -1,37 +1,57 @@
 /**Selectores */
-const contenedorProductos = document.querySelector("#lista-productos")
+const contenedorProductos = document.querySelector("#lista-productos");
 const tbody = document.querySelector("#lista-carrito tbody")
+const btnVaciarCarrito = document.querySelector("#vaciar-carrito");
 const btnToggleMode = document.querySelector("#btnToggleMode")
-/**Inicio mi lista de carrito de compras */
-let carritoCompras = []
+/**Creo la lista para guardar los items que estarán dentro del carrito de compras */
+let listaCarrito = [];
 
 /**Eventos */
+btnVaciarCarrito.addEventListener("click", (event) => {
+    event.preventDefault();
+    listaCarrito = []
+    limpiarCarrito();
+})
 
 btnToggleMode.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
 })
 
+
 contenedorProductos.addEventListener("click", function (event) {
+    /**Elimino los eventos por defecto */
+    event.preventDefault();
 
-    /**Validamos que el elemento al que se le dio clic
-     * contenga la clase agregar-carrito
-    */
+    /**Validar que el elemento HTML contenga las clases agregar-carrito */
     if (event.target.classList.contains("agregar-carrito")) {
-        /**Obtengo el id del producto con la propiedad
-        getAttribute */
-        const id = event.target.getAttribute("data-id")
-        /**Subir hasta el padre dos veces */
-        const card = event.target.parentElement.parentElement;
-        /**Invoco a la función agregar carrito */
-        agregarProductoCarrito(id, card);
-    }
 
+        /**Extraer el atributo personalizado que contiene el id del producto */
+        const id = event.target.getAttribute("data-id")
+        /**Obtengo el padre mas alto para poder obtener la información */
+        const card = event.target.parentElement.parentElement;
+
+        agregarProducto(id, card)
+    }
 })
 
-function agregarProductoCarrito(id, card) {
+tbody.addEventListener("click", (event) => {
+    event.preventDefault();
+    /**Valido si el usuario está dando clic a una etiqueta que
+     * contenga la clase "eliminar producto"
+     */
+    if (event.target.classList.contains("eliminar-producto")) {
+        /**Obtengo el id de la etiqueta */
+        const id = event.target.getAttribute("data-id")
+        eliminarItemCarrito(id);
+    }
+})
 
-    /**Creo un objeto con toda la información del producto */
-    const infoProducto = {
+/**Esta función obtiene los datos y agrega el producto a la lista */
+function agregarProducto(id, card) {
+    /**Creo el objeto con la información seleccionando las
+     *  etiquetas a partir de card
+     */
+    infoProducto = {
         id,
         nombre: card.querySelector("h4").textContent,
         precio: card.querySelector(".precio span").textContent,
@@ -39,87 +59,95 @@ function agregarProductoCarrito(id, card) {
         cantidad: 1
     }
 
-    /**Busco al producto que contenga el id dentro de
-     * la lista del carrito
-     */
-    const producto = carritoCompras.find(elemento => elemento.id == id);
-
-    /**Si el producto existe entonces */
-    if (producto) {
-        /**Aumento la cantidad */
-        producto.cantidad++
+    /**Validar si el producto ya se encuentra en la lista del carrito */
+    if (listaCarrito.some(producto => producto.id == id)) {
+        /**Obtengo el valor del producto */
+        const producto = listaCarrito.find(producto => producto.id == id)
+        //Aumento su cantidad 
+        producto.cantidad++;
     } else {
-        /**De lo contrario lo agrego a la lista del carrito */
-        carritoCompras.push(infoProducto)
+        //De lo contrario agregamos el producto al carrito
+        listaCarrito.push(infoProducto);
+    }
+    console.log(listaCarrito)
+    pintarProductosEnCarrito();
+}
+
+/**Esta función recorre todos los productos
+ * guardados en la lista del carrito y los muestra
+ */
+function pintarProductosEnCarrito() {
+    // tbody.innerHTML = ""
+    limpiarCarrito()
+    //**Recorremos la lista del carrito */
+    listaCarrito.forEach((producto) => {
+        const tr = document.createElement("tr");
+
+        /**Columna Imagen */
+        const tdImage = document.createElement("td");
+        const img = document.createElement("img");
+        img.src = producto.imagen;
+        img.alt = "Imagen del prodcuto";
+        img.height = "100";
+        tdImage.appendChild(img);
+        tr.appendChild(tdImage);
+
+
+        /**Columna nombre */
+        const tdName = document.createElement("td")
+        tdName.textContent = producto.nombre;
+        tr.appendChild(tdName);
+
+
+        /**Columna precio */
+        const tdPrecio = document.createElement("td");
+        tdPrecio.textContent = producto.precio;
+        tr.appendChild(tdPrecio);
+
+        /**Columna cantidad */
+        const tdCantidad = document.createElement("td")
+        tdCantidad.textContent = producto.cantidad;
+        tr.appendChild(tdCantidad);
+
+        /**Columna para el botón eliminar */
+        const tdEliminar = document.createElement("td")
+        const btnEliminar = document.createElement("button")
+        btnEliminar.textContent = "Eliminar";
+        btnEliminar.setAttribute("data-id", producto.id);
+        btnEliminar.classList.add("eliminar-producto");
+        tdEliminar.appendChild(btnEliminar);
+        tr.appendChild(tdEliminar);
+
+        tbody.appendChild(tr)
+
+    })
+
+    console.log(tbody)
+}
+
+/**Esta función se encarga de eliminar un producto si la cantidad es igual 1, de lo contrario resta la cantidad en una unidad. */
+function eliminarItemCarrito(id) {
+    /**Obtengo el producto con el id que tengo como parametro */
+    const producto = listaCarrito.find(producto => producto.id == id)
+
+    /**Si la cantidad del producto es mayor a 1 */
+    if (producto.cantidad > 1) {
+        //Se resta la cantidad
+        producto.cantidad--;
+    } else {
+        //De lo contrario, filtro todos los productos 
+        //Que no contengan ese identidicador en especifico
+        listaCarrito = listaCarrito.filter((producto) => producto.id != id)
     }
 
     pintarProductosEnCarrito()
 }
 
-
-function pintarProductosEnCarrito() {
-    //Forma lenta de limpiar
-    // tbody.innerHTML = ""
-    limpiarCarrito();
-    // carritoCompras.forEach(function (producto) {
-    //     tbody.innerHTML += `
-    //         <tr>
-    //             <td>
-    //                 <img height="100" src="${producto.imagen}"/>
-    //             </td>
-    //             <td>${producto.nombre}</td>
-    //             <td>${producto.precio}</td>
-    //             <td>${producto.cantidad}</td>
-    //             <td>
-    //                 <button class="eliminar-producto" data-id="${producto.id}">Eliminar</button>
-    //             </td>
-    //         </tr>
-    //     `;
-    // })
-
-    carritoCompras.forEach(function (producto) {
-        const tr = document.createElement("tr");
-
-        //TD DE LA IMAGEN
-        const tdImagen = document.createElement("td");
-        const imagen = document.createElement("img");
-        imagen.src = producto.imagen;
-        imagen.height = "100";
-
-        tdImagen.appendChild(imagen)
-        tr.appendChild(tdImagen);
-
-        //TD para el nombre
-        const tdNombre = document.createElement("td")
-        tdNombre.textContent = producto.nombre;
-        tr.appendChild(tdNombre);
-
-        //TD PARA EL PRECIO
-        const tdPrecio = document.createElement("td")
-        tdPrecio.textContent = producto.precio;
-        tr.appendChild(tdPrecio)
-
-        //TD PARA la cantidad
-        const tdCantidad = document.createElement("td")
-        tdCantidad.textContent = producto.cantidad;
-        tr.appendChild(tdCantidad)
-
-        //TD para el boton
-        const tdBoton = document.createElement("td")
-        const boton = document.createElement("button");
-        boton.classList.add("eliminar-producto")
-        boton.setAttribute("data-id", producto.id)
-        boton.textContent = "Eliminar";
-
-        tdBoton.appendChild(boton)
-        tr.appendChild(tdBoton);
-
-        tbody.appendChild(tr)
-    })
-}
-
+/**Funcion para limpiar el carrito de forma más rapida */
 function limpiarCarrito() {
+    //Mientras encuentre un hijo
     while (tbody.firstChild) {
+        //Lo elimina
         tbody.removeChild(tbody.firstChild)
     }
 }

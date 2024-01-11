05edb45240d1d9920d1d97d644ff4b710aa07bcf
Kevin Mejia <kwmejia9@gmail.com>
2024-01-11 12:03:03 -0500

    Riwi ecologico

--- 16 - Riwi_Ecologico/js/app.js
+++ 16 - Riwi_Ecologico/js/app.js
@@ -32,6 +32,11 @@ const btnAgregar = document.getElementById("btnSubmit")
 
 //Eventos y escuchadores de eventos
 document.addEventListener("DOMContentLoaded", () => {
+
+    const puntosCache = localStorage.getItem("puntosEcologicos")
+    if (puntosCache) {
+        puntosEcologicos = JSON.parse(puntosCache)
+    }
     pintarPuntoEcologico();
 })
 
@@ -40,9 +45,8 @@ selectPiso.addEventListener("input", () => {
 })
 
 btnAgregar.addEventListener("click", () => {
-    const cantidad = document.getElementById("cantidad").value
+    const cantidad = document.getElementById("cantidad")
 
-    console.log(cantidad, selectPiso.value, tipoCanecaAgregar)
     //Agregar la cantidad de desechos en su respectivo punto ecologico y piso
 
     //1. Recorrer la lista de puntos ecologicos
@@ -53,7 +57,20 @@ btnAgregar.addEventListener("click", () => {
             //1.aprovechables
             //2.no_aprovechables
             //3.organicos
-            punto[tipoCanecaAgregar] += parseInt(cantidad)
+            if (punto[tipoCanecaAgregar] + parseInt(cantidad.value) <= 500) {
+                punto[tipoCanecaAgregar] += parseInt(cantidad.value)
+                //Cierro el modal
+                document.getElementById("btnCloseModal").click();
+                //Limpiar el input de cantidad
+                cantidad.value = "";
+
+            } else {
+                const parrafo = document.createElement("p")
+                parrafo.textContent = "La cantidad es excesiva."
+                parrafo.style.color = "red"
+                cantidad.after(parrafo)
+
+            }
         }
     })
 
@@ -73,9 +90,39 @@ canecas.forEach(caneca => {
 //Funciones
 function pintarPuntoEcologico() {
     //1. Recorrer la lista de puntos ecologicos
+    puntosEcologicos.forEach(punto => {
 
-    //1.2 Preguntar si el piso que se está iterando es igual al seleccionado por el usuario
-    //1.3 Seleccionar los contadores de cada caneca y asignar nuevo valor
-    //1.4 Sacar el promedio del punto ecologico 
+        //1.2 Preguntar si el piso que se está iterando es igual al seleccionado por el usuario
+        if (punto.piso == selectPiso.value) {
+
+            //1.3 Seleccionar los contadores de cada caneca y asignar nuevo valor
+
+            const contadorOrganico = document.querySelector("#organicos .body_top span")
+            const contadorNoAprovechable = document.querySelector("#no_aprovechables .body_top span")
+            const contadorAprovechable = document.querySelector("#aprovechables .body_top span")
+
+            contadorOrganico.textContent = `${punto.organicos}/500`;
+            contadorNoAprovechable.textContent = `${punto.no_aprovechables}/500`;
+            contadorAprovechable.textContent = `${punto.aprovechables}/500`;
+
+            //1.4 Sacar el promedio del punto ecologico 
+            const suma = punto.aprovechables + punto.no_aprovechables + punto.organicos;
+            const promedio = (suma * 100) / 1500;
+
+            if (promedio < 25) {
+                document.body.style.background = "#950101";
+            } else if (promedio < 50) {
+                document.body.style.background = "#fc4b08";
+
+            } else {
+                document.body.style.background = "#00f839";
+
+            }
+
+            console.log(promedio)
+        }
+    })
+    //GUARDAMOS LA INFORMACIÓN EN EL LOCAL STORAGE
+    localStorage.setItem("puntosEcologicos", JSON.stringify(puntosEcologicos));
 
 }

57e62a681de5f15bbd0981e7a530a99dd13e1d0b
Kevin Mejia <kwmejia9@gmail.com>
2024-01-19 11:48:07 -0500

    pelicula

--- 21 - Peliculas API/js/app.js
+++ 21 - Peliculas API/js/app.js
@@ -3,7 +3,20 @@ const inputTitle = document.querySelector("#search")
 const container = document.querySelector(".container-cards");
 
 let timer
+
+
 //Eventos
+container.addEventListener("click", (event) => {
+
+    if (event.target.classList.contains("btn-show-more")) {
+        const id = event.target.getAttribute("id-movie")
+        console.log(id)
+        loadShowMore(id)
+    }
+
+})
+
+
 inputTitle.addEventListener("input", (event) => {
     //Event = evento que ocurrió
     //target = la etiqueta donde ocurrió el evento
@@ -32,7 +45,87 @@ async function callToApi(title) {
 }
 
 //Función para mostrar las peliculas en el HTML
+
+async function loadShowMore(id) {
+    const URL = `https://www.omdbapi.com/?apikey=690d22ef&i=${id}`
+    try {
+        const response = await fetch(URL)
+        const data = await response.json()
+        console.log(data)
+        //Funcion para imprimir en el HTML los detalles de la pelicula
+        printShowMore(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+function printShowMore(movie) {
+
+    //Desestructuración del objeto movie
+
+    //1. Primera forma de acceder a un objet
+    // movie.Poster
+
+    //2. Segunda forma = movie["Poster"]
+
+    //3. const { Poster: imagen, DVD, Actors } =  movie;
+
+
+    // const imagen = movie.Poster
+    const {
+        Poster,
+        Language,
+        Genre,
+        Director,
+        Country,
+        BoxOffice,
+        Actors,
+        Title,
+        Writer,
+        Year,
+        Released,
+        Plot
+    } = movie
+
+    cleanHTML();
+
+    container.innerHTML = `
+      <div class="card-show-more">
+        <i class='bx bx-arrow-back'></i>
+        <img  src="${Poster}"/>
+
+        <div> 
+            <p>Title: <span>${Title}</span></p>
+            <p>Director: <span>${Director}</span></p>
+            <p>Genre: <span>${Genre}</span></p>
+            <p>Language: <span>${Language}</span></p>
+            <p>Year: <span>${Year}</span></p>
+            <p>Writer: <span>${Writer}</span></p>
+            <p>Released: <span>${Released}</span></p>
+            <p>Country: <span>${Country}</span></p>
+            <p>Actors: <span>${Actors}</span></p>
+            <p>Plot: <span>${Plot}</span></p>
+            <p>BoxOffice: <span>${BoxOffice}</span></p>
+        </div>
+      </div>
+    `
+}
+
+
 function printMovies(movies) {
+    //Limpiar el HTML
+    cleanHTML()
+
+    //Validar que si existan peliculas
+    if (!movies) {
+        const titleError = document.createElement("h2")
+        titleError.textContent = "La pelicula no fue encontrada 😒";
+        titleError.classList.add("alert")
+
+        container.appendChild(titleError)
+    }
+
     //Recorremos la lista
     movies.forEach(movie => {
         //Inyectar el HTML
@@ -46,9 +139,16 @@ function printMovies(movies) {
 
                 <p>Año: <span>${movie.Year}</span></p>
                 <p>Tipo: <span>${movie.Type}</span></p>
-                <button>Ver más</button>
+                <button class="btn-show-more" id-movie="${movie.imdbID}">Ver más</button>
             </div>
         `;
 
     });
+}
+
+
+function cleanHTML() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
 }
